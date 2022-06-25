7d93a3bedd79d93cffbb396a0d8c8bba70629408
NILAY SARDAR <nilay.sardar04121998@gmail.com>
2022-06-25 04:14:23 -0700

    basic code structure of c language

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Here (unsigned) is modifier, (int) is data type, (c) is variable and identifier, and 10 is assigned value to c.
+    unsigned  int c = 10;
+
+    printf("C contain value of %d", c);
+    return 0;
+}
